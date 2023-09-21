18acd7041f01adb3e25124f83cdc177d12072764
levai-mihaly <levaimihalyzoltan@gmail.com>
2023-09-21 13:00:07 +0200

    hibak fellelese

--- main.cpp
+++ main.cpp
@@ -4,21 +4,21 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
+    int *b = new int[NELEMENTS]; // _ typo 
+    std::cout << '1-100 ertekek duplazasa'  // '' helyett "" és ; hiánya
     for (int i = 0;)
     {
         b[i] = i * 2;
     }
     for (int i = 0; i; i++)
     {
-        std::cout << "Ertek:"
+        std::cout << "Ertek:" // ; hiánya
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
     int atlag;
     for (int i = 0; i < N_ELEMENTS, i++)
     {
-        atlag += b[i]
+        atlag += b[i] // ; hiánya
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
