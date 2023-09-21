442ff5ef6bbb0aa5085102ef6306570cc68741d5
levai-mihaly <levaimihalyzoltan@gmail.com>
2023-09-21 13:33:39 +0200

    tovabbi hibak fellelese es javitasa

--- main.cpp
+++ main.cpp
@@ -4,21 +4,21 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS]; // _ typo 
-    std::cout << '1-100 ertekek duplazasa'  // '' helyett "" és ; hiánya
-    for (int i = 0;)
+    int *b = new int[N_ELEMENTS]; // _ typo 
+    std::cout << "1-100 ertekek duplazasa" << '\n';  // '' helyett "" és ; hiánya
+    for (int i = 0; i < N_ELEMENTS; i++) // for loop javítása
     {
-        b[i] = i * 2;
+        b[i] = (i + 1) * 2; // 1-100-ig duplazzuk az ertekeket, nem 0-99-ig 
     }
-    for (int i = 0; i; i++)
+    for (int i = 0; i < N_ELEMENTS; i++) // for loop javítása 
     {
-        std::cout << "Ertek:" // ; hiánya
+        std::cout << i + 1 << ". Ertek: " << b[i] << '\n'; // ; hiánya, tömb elemeinek kiírása
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
     int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++)
+    for (int i = 0; i < N_ELEMENTS; i++) // for loop javítása
     {
-        atlag += b[i] // ; hiánya
+        atlag += b[i]; // ; hiánya
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
